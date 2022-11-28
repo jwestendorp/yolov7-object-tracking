6bee70c5e750cef63386ca427565e52b51401288
Jonas <tuberjonas@gmail.com>
2022-11-28 01:32:06 +0100

    at least working

--- crop.py
+++ crop.py
@@ -8,56 +8,45 @@ from utils.datasets import LoadImages
 currentFolder = Path().cwd()
 
 # filePath = str(currentFolder / 'runs' / 'detect' / 'video.mp4' / 'apple-5.csv')
-filePath = str(currentFolder / 'runs/detect/video.mp4/apple-5.csv')
+filePath = str(currentFolder / 'runs/detect/video.mp4/bird-0.csv')
 videoSrc = str(currentFolder / 'video.mp4')
 video = cv2.VideoCapture('video.mp4')
 
+save_path = str(currentFolder / 'render/output.mp4')
 
-# while video.isOpened():
-#     ret, frame = video.read()
-#     # if frame is read correctly ret is True
-#     if not ret:
-#         print("Can't receive frame (stream end?). Exiting ...")
-#         break
-#     cv2.imshow('frame', frame)
-#     if cv2.waitKey(1) == ord('q'):
-#         break
-# video.release()
-# cv2.destroyAllWindows()
+vid_path, vid_writer = None, None
+
+fps = video.get(cv2.CAP_PROP_FPS)
+w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+vid_writer = cv2.VideoWriter(
+    save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+# vid_writer = cv2.VideoWriter(
+#     save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
 
 with open(filePath, newline='') as csvfile:
-    rows = csv.reader(csvfile, delimiter=',')
+    values = csv.reader(csvfile, delimiter=',')
     data = []
-    for row in rows:
+    rows = list(values)
+    for i, row in enumerate(rows):
         # data.append(row)
         frameNr = int(row[0])
-        x = float(row[1])
-        y = float(row[2])
-        w = float(row[3])
-        h = float(row[4])
+        x1 = int(row[1])
+        x2 = int(row[2])
+        y1 = int(row[3])
+        y2 = int(row[4])
 
         video.set(cv2.CAP_PROP_POS_FRAMES, frameNr)
         ret, frame = video.read()
 
         outputFrame = np.zeros(frame.shape, dtype="uint8")
-
-        y_res = frame.shape[0]
-        x_res = frame.shape[1]
-
-        print(x_res, y_res)
-
-        width = w * x_res
-        height = h * y_res
-
-        y1 =
-        y2 =
-        x1 =
-        x2 =
-
         outputFrame[y1:y2, x1:x2] = frame[y1:y2, x1:x2]
 
-        cv2.imshow('frame', frame)
-        cv2.waitKey(1)
+        # cv2.imshow('frame', outputFrame)
+        vid_writer.write(outputFrame)
+        # cv2.waitKey(1)
+        print(i, '/', len(rows)-1)
 
-video.release()
-cv2.destroyAllWindows()
+# video.release()
+# cv2.destroyAllWindows()
